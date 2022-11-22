842428718165438bd1c3a567e5e25c95ed342f4b
RuoYi <yzz_ivy@163.com>
2022-11-22 09:24:34 +0800

    修复某些特性的环境生成代码变乱码TXT文件问题

--- src/views/tool/gen/index.vue
+++ src/views/tool/gen/index.vue
@@ -222,7 +222,7 @@ function handleGenTable(row) {
       proxy.$modal.msgSuccess("成功生成到自定义路径：" + row.genPath);
     });
   } else {
-    proxy.$download.zip("/code/gen/batchGenCode?tables=" + tbNames, "ruoyi");
+    proxy.$download.zip("/code/gen/batchGenCode?tables=" + tbNames, "ruoyi.zip");
   }
 }
 /** 同步数据库操作 */
