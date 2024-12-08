728dbfc0535b6371e1674d4b80c0d93e4c890539
全易 <771184240@qq.com>
2024-12-08 14:41:59 +0800

    优化全局请求目录结构

--- src/api/system/dict/type.js
+++ src/api/system/dict/type.js
@@ -1,4 +1,4 @@
-import request from '@/utils/request'
+import request from '@/service/index.js'
 
 // 查询字典类型列表
 export function listType(query) {
--- src/main.js
+++ src/main.js
@@ -16,7 +16,7 @@ import directive from './directive' // directive
 
 // 注册指令
 import plugins from './plugins' // plugins
-import { download } from '@/utils/request'
+import { download } from '@/service/index.js'
 
 // svg图标
 import 'virtual:svg-icons-register'
