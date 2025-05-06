6d33478d757b064e7f283a79d1c21affdd09812f
RuoYi <yzz_ivy@163.com>
2025-05-06 19:15:24 +0800

    图片上传组件新增disabled属性

--- src/components/ImageUpload/index.vue
+++ src/components/ImageUpload/index.vue
@@ -2,6 +2,7 @@
   <div class="component-upload-image">
     <el-upload
       multiple
+      :disabled="disabled"
       :action="uploadImgUrl"
       list-type="picture-card"
       :on-success="handleUploadSuccess"
@@ -21,7 +22,7 @@
       <el-icon class="avatar-uploader-icon"><plus /></el-icon>
     </el-upload>
     <!-- 上传提示 -->
-    <div class="el-upload__tip" v-if="showTip">
+    <div class="el-upload__tip" v-if="showTip && !disabled">
       请上传
       <template v-if="fileSize">
         大小不超过 <b style="color: #f56c6c">{{ fileSize }}MB</b>
@@ -80,6 +81,11 @@ const props = defineProps({
   isShowTip: {
     type: Boolean,
     default: true
+  },
+  // 禁用组件（仅查看图片）
+  disabled: {
+    type: Boolean,
+    default: false
   },
   // 拖动排序
   drag: {
@@ -220,7 +226,7 @@ function listToString(list, separator) {
 
 // 初始化拖拽排序
 onMounted(() => {
-  if (props.drag) {
+  if (props.drag && !props.disabled) {
     nextTick(() => {
       const element = proxy.$refs.imageUpload?.$el?.querySelector('.el-upload-list')
       Sortable.create(element, {
@@ -240,4 +246,8 @@ onMounted(() => {
 :deep(.hide .el-upload--picture-card) {
     display: none;
 }
+
+:deep(.el-upload.el-upload--picture-card.is-disabled) {
+  display: none !important;
+} 
 </style>
