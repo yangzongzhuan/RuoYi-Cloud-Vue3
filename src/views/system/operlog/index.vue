c8000fa42e5cc9a116d0ec70b85e7915a115a0fe
RuoYi <yzz_ivy@163.com>
2026-04-14 16:45:55 +0800

    通知公告新增阅读用户&详细

--- src/views/system/operlog/index.vue
+++ src/views/system/operlog/index.vue
@@ -154,7 +154,7 @@
 </template>
 
 <script setup lang="ts" name="Operlog">
-import OperlogDetail from './detail'
+import OperlogDetail from './detail.vue'
 import { list, delOperlog, cleanOperlog } from "@/api/system/operlog"
 import type { SysOperLog, OperlogQueryParams } from '@/types/api/system/operlog'
 
